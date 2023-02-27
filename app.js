eef96f9b7552eb944a94bc3c4a37c4200ffd8fa2
Rahul9.Kamble <rahul9.kamble@ril.com>
2023-02-27 18:49:08 +0530

    Basic Authentication

--- app.js
+++ app.js
@@ -37,6 +37,34 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+function auth (req, res, next){
+  console.log(req.headers);
+  var authHeader = req.headers.authorization;
+  if(!authHeader){
+      var err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      next(err);
+      return;
+  }
+  
+  var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+  var user = auth[0];
+  var pass = auth[1];
+
+  if(user === 'admin' && pass==='password'){
+    next();
+  }else{
+      var err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      next(err); 
+  }
+
+}
+
+app.use(auth);
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/dishes', dishRouter);
